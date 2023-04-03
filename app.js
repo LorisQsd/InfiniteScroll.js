d15acac2451218e03ab3f79084d6cc7d195eb911
LorisQ <121116287+LorisQsd@users.noreply.github.com>
2023-04-03 15:15:28 +0200

    CSS Responsive improved / JS
    - Add a smoother scroll to top
    - Improve CSS responsive
    - Less JS, (delete the result array method to keep the searchinput.value in memory => instead, create a search let variable to keep the result)
    - Add infinite scroll on the random query (remove the Math.random method to make it simplier in the api request.)

--- app.js
+++ app.js
@@ -1,129 +1,78 @@
 /* API KEY : _dRyG2MN1OKFUEXnduMCucDHGyxhxJmKp_Qt08Dtmx0*/
 const errorMsg = document.querySelector("#errorMsg");
 const resultsDisplay = document.querySelector(".gallery");
+let searchUrl = "random";
+let page = 1;
 
-const randomPage = Math.trunc(Math.random() * 100)
-const randomUrl = `https://api.unsplash.com/photos?random&page=${randomPage}&per_page=20&client_id=_dRyG2MN1OKFUEXnduMCucDHGyxhxJmKp_Qt08Dtmx0`;
-
-randomAPICall();
-async function randomAPICall() {
+initAPICall();
+async function initAPICall() {
     try {
-        const response = await fetch(randomUrl)
+        const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&per_page=20&query=${searchUrl}&client_id=_dRyG2MN1OKFUEXnduMCucDHGyxhxJmKp_Qt08Dtmx0`)
+
         if (!response.ok) {
             throw new Error(`${response.status}`);
         }
 
         const data = await response.json();
 
-        data.forEach(el => {
-            const elem = document.createElement("img");
-            elem.setAttribute("src", el.urls.small);
-            resultsDisplay.appendChild(elem);
-        });
+        console.log(data)
 
+        if (!data.total) {
+            resultsDisplay.textContent = "";
+            throw new Error("Désolé, je n'ai trouvé aucun résultat... Soyez plus précis dans votre recherche.") ;
+        }
+
+        createImages(data.results)
+        
     } catch (error) {
         errorMsg.textContent = `${error}`
     }
 }
 
-// SEARCH IMG
-const searchInput = document.querySelector("input");
-const form = document.querySelector("form");
-const footer = document.querySelector("footer");
-
-form.addEventListener("submit", handleSubmit)
-
-let result = [];
-async function handleSubmit(e) {
-    e.preventDefault();
-
-    if (searchInput.value === "") {
-        errorMsg.textContent = "Veuillez remplir un terme valide";
-        return;
-    } else {
-        result = [];
-        footer.style.display = "none";
-        errorMsg.textContent = "";
-        resultsDisplay.textContent = "";
-        searchAPICall(searchInput.value);
-        result.push(searchInput.value);
-    }
-}
-
-let page = 1;
-
-async function searchAPICall(searchInput) {
-    try {
-        const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${searchInput}&client_id=_dRyG2MN1OKFUEXnduMCucDHGyxhxJmKp_Qt08Dtmx0`);
-
-        if (!response.ok) {
-            throw new Error(`${response.status}`)
-        }
-
-        const data = await response.json();
-
-        displaySearchResult(data.results);
-        
-        page++;
-
-    } catch (error) {
-        errorMsg.textContent = `${error}`;
-    }
-}
-
-function displaySearchResult(data) {
-    if (!data.length) {
-        errorMsg.textContent = "Désolé, je n'ai trouvé aucun résultat... Soyez plus précis dans votre recherche.";
-        return;
-    }
-
-    footer.style.display = "block";
+function createImages(data) {
     data.forEach(el => {
         const elem = document.createElement("img");
-        elem.setAttribute("src", el.urls.small);
+        elem.setAttribute("src", el.urls.regular);
         resultsDisplay.appendChild(elem);
     });
-
 }
 
 //INTERSECTION OBSERVER TO SET INFINITE SCROLL
+const footer = document.querySelector("footer");
 const options = {
     root: null,
     threshold: 0
 };
 
 const observer = new IntersectionObserver(function (entries) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            infiniteAPICall();
+        if (window.scrollY > window.innerHeight && entries[0].isIntersecting) {
+            page++;
+            initAPICall();
         }
-    })
 }, options)
 
 observer.observe(footer);
 
-async function infiniteAPICall() {
-    try {
-        const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&per_page=20&query=${result[0]}&client_id=_dRyG2MN1OKFUEXnduMCucDHGyxhxJmKp_Qt08Dtmx0`);
+// SEARCH IMG
+const searchInput = document.querySelector("#search");
+const form = document.querySelector("form");
 
-        if (!response.ok) {
-            throw new Error(`${response.status}`)
-        }
+form.addEventListener("submit", handleSearch)
 
-        const data = await response.json();
+async function handleSearch(e) {
+    e.preventDefault();
 
-        displaySearchResult(data.results);
-
-        if (page > data.total_pages) {
-            errorMsg.textContent = "Il n'y a plus de contenu à afficher";
-            return;
-        }
-
-        page++;
-
-    } catch (error) {
-        errorMsg.textContent = `${error}`;
+    resultsDisplay.textContent = "";
+    if (searchInput.value === "") {
+        errorMsg.textContent = "Veuillez remplir un terme valide";
+        return;
     }
+
+    errorMsg.textContent = "";
+    resultsDisplay.textContent = "";
+    searchUrl = searchInput.value;
+    page = 1;
+    initAPICall();
 }
 
 //SCROLL TOP
@@ -131,5 +80,8 @@ const btn = document.querySelector(".go-up");
 btn.addEventListener("click", scrollTop);
 
 function scrollTop() {
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
 }
